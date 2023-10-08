a094c8cef408aa54ab35f16cec704a9c68383c15
Gathukumi <gathukumidavid001@gmail.com>
2023-10-08 17:34:23 +0300

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // Your code goes here
+
+  // You can access and manipulate DOM elements
+  const addButton = document.getElementById("add-button");
+  const taskList = document.getElementById("task-list");
+
+  addButton.addEventListener("click", () => {
+    // Code to add a new task to the task list
+    const newTask = document.createElement("li");
+    newTask.textContent = "New Task";
+    taskList.appendChild(newTask);
+  });
+
+  // More code for other features or enhancements
 });
